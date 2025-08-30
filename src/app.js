36add9d64f3a3c3f38b2b43a47bd827e5958fb85
shubham123aws <100993250+shubham123aws@users.noreply.github.com>
2025-08-30 04:54:11 -0700

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Shubham 🚀</h1>
+      <p>React app running inside Docker (multi-stage build)</p>
+    </div>
+  );
+}
+
+export default App;
